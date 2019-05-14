35b20f1cbcdf9eaad8a102faaa780fe5ddc79ffd
Johan <joh.pik.fr@gmail.com>
2019-05-15 06:40:30 +1000

    Make request to jsonplaceholder with axios

--- src/components/RessourceList.js
+++ src/components/RessourceList.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import axios from 'axios'
 
 class RessourceList extends React.Component {
 
-  render(){
-console.log(this.props);
-    return(
-      <div>{this.props.ressource}</div>
-    )
+  state = { ressources: [] }
 
+  async componentDidMount(){
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.ressource}`)
+    this.setState({ ressources: response.data })
+  }
+
+  async componentDidUpdate(prevProps){
+    if (prevProps.ressource !== this.props.ressource) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.ressource}`)
+      this.setState({ ressources: response.data })
+    }
+    return
+  }
+
+
+  render(){
+    return(
+      <div>{this.state.ressources.length}</div>
+    )
   }
 
 }
